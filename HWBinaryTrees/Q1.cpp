7bb324cc9198e4276772c9c2261f6387bb2a5019
Hector Corpus <hectorcorpus121505@gmail.com>
2025-09-28 16:29:06 -0500

    Actually Followed Instructions

--- HWBinaryTrees/Q1.cpp
+++ HWBinaryTrees/Q1.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int binarySearch(int arr[], int L, int R, int x)
+
+int fastFind(vector<double> &A, double key)
 {
-    if (L > R)
-        return -1;
-    int M = L + (R - L) / 2;
-    if (arr[M] == x)
-        return M;
-    if (arr[M] > x)
-        return binarySearch(arr, L, M - 1, x);
-    return binarySearch(arr, M + 1, R, x);
+    int L = 0;
+    int R = A.size() - 1;
+    while(L <= R)
+    {
+        int M = L + (R - L) / 2;
+        if (A[M] == key)
+            return M;
+        else if (A[M] > key)
+            R = M - 1;
+        else
+            L = M + 1;
+    }
+    return -1;
 }
 
 int main() {
-    int array[] = {1, 4, 8, 10, 20};
-    cout << binarySearch(array, 0, 4, 10) << endl;
+    vector<double> list = {1, 4, 8, 10, 20};
+    cout << fastFind(list, 5) << endl;
     return 0;
 }
