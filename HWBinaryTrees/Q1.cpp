7581856962995be8c83aa6e90a1107e4ed5dd079
Hector Corpus <hectorcorpus121505@gmail.com>
2025-09-29 12:07:00 -0500

    added comments to HW Binary Tree

--- HWBinaryTrees/Q1.cpp
+++ HWBinaryTrees/Q1.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int L, int R, int x)
+int binarySearch(int arr[], int L, int R, int key)
 {
+    //base case if right and left point intersect
     if (L > R)
         return -1;
+    //find the center of the array
     int M = L + (R - L) / 2;
-    if (arr[M] == x)
+    //checks if point in center of left and right point is key
+    if (arr[M] == key)
         return M;
-    if (arr[M] > x)
-        return binarySearch(arr, L, M - 1, x);
-    return binarySearch(arr, M + 1, R, x);
+    //if key is less then the middle, then changes search range to be to left side
+    if (arr[M] > key)
+        return binarySearch(arr, L, M - 1, key);
+    //else changes range to right side
+    return binarySearch(arr, M + 1, R, key);
 }
 
 int main() {
