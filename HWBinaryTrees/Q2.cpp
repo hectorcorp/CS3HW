0d5c1ffd84b0102b330199c04100dfd1d541b523
Hector Corpus <hectorcorpus121505@gmail.com>
2025-09-30 19:28:54 -0500

    update

--- HWBinaryTrees/Q2.cpp
+++ HWBinaryTrees/Q2.cpp
@@ -8,11 +8,11 @@ node* buildTree(vector<double> & A, int start, int end)
     if (end < start)
         return nullptr;
     int mid = (start + end) / 2;
-    node * no = new node;
-    no -> data = A[mid];
-    no -> left = buildTree(A, start, mid - 1);
-    no -> right = buildTree(A, mid + 1, end);
-    return no;
+    node * n = new node;
+    n -> data = A[mid];
+    n -> left = buildTree(A, start, mid - 1);
+    n -> right = buildTree(A, mid + 1, end);
+    return n;
 }
 
 
