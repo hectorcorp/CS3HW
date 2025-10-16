bf2fde4780b1f725582cb35c659bb86dc2d94cb3
Hector Corpus <hectorcorpus121505@gmail.com>
2025-10-16 17:39:34 -0500

    Finsihed HWAC2

--- HWAC2/autocompleter.cpp
+++ HWAC2/autocompleter.cpp
@@ -15,49 +15,92 @@ Autocompleter::Autocompleter()
 //
 // Must run in O(1) time.
 
+//instead of updating the n top first and then moving to next one, moving to first checking the root top first, then in loop updating n then updating top works.
 void Autocompleter::insert(string x, int freq)
 {
     Node* n = root;
-    Entry e;
-    e.freq = freq;
-    e.s = x;
-    for(char a: x)
+
+    
+    bool inVect = false;
+    //go throught top vector
+    for(int i = 0; i < n->top.size(); i++)
     {
-        bool inserted = false;
-        for(int i = 0; i < n->top.size(); i++)
+        if(n->top[i].s == x)
+            inVect = true;
+    }
+    //item not in current top and adds it and sorts it
+    if(!inVect)
+    {
+        Entry e;
+        e.s = x;
+        e.freq = freq;
+        n->top.push_back(e);
+        int size = n->top.size();
+        for(int j = 0; j < size; j++)
         {
-            if(e.s == n->top[i].s)
-            { 
-                inserted = true;
-                break;
-            }
-            else if(e.freq > n->top[i].freq)
+            for(int k = 0; k < size - j - 1; k++)
             {
-                n->top.insert(n->top.begin() + i, e);
-                inserted = true;
-                break;
-            }     
-        }  
-        if(!inserted) 
-            n->top.push_back(e);   
-
-
+                if(n->top[k].freq < n->top[k + 1].freq)
+                {
+                    Entry temp = n->top[k];
+                    n->top[k] = n->top[k + 1];
+                    n->top[k + 1] = temp;
+                }
+            }
+        }
         if(n->top.size() > 3)
             n->top.pop_back();
-
-        
-        if(n->children[int(a)] == nullptr)
-            n->children[int(a)] = new Node();
-
-        n = n->children[int(a)];
     }
 
-    if(n->marked) 
+    
+    for (int a : x)
+    {
+        //n needs to go throught each char and find the int(a) to it.
+        //while it in its node update itself to correct sorted location on top 3 of the vector
+        //if vector is more than 3 pop_back
+        //dont crash :)
+        
+        if(n->children[a] == nullptr)
+            n->children[a] = new Node();
+        n = n->children[a];
+
+        bool inVect = false;
+        //go throught top vector
+        for(int i = 0; i < n->top.size(); i++)
+        {
+            if(n->top[i].s == x)
+                inVect = true;
+        }
+        //item not in current top and adds it and sorts it
+        if(!inVect)
+        {
+            Entry e;
+            e.s = x;
+            e.freq = freq;
+            n->top.push_back(e);
+            int size = n->top.size();
+            for(int j = 0; j < size; j++)
+            {
+                for(int k = 0; k < size - j - 1; k++)
+                {
+                    if(n->top[k].freq < n->top[k + 1].freq)
+                    {
+                        Entry temp = n->top[k];
+                        n->top[k] = n->top[k + 1];
+                        n->top[k + 1] = temp;
+                    }
+                }
+            }
+            if(n->top.size() > 3)
+                n->top.pop_back();
+        }
+        
+    }
+    if(n->marked)
         return;
     n->marked = true;
     count++;
 }
-
 
 // Returns the number of strings in the dictionary.
 // 
@@ -77,12 +120,11 @@ void Autocompleter::completions(string x, vector<string> &T)
 {
     T.clear();
     Node* n = root;
-    for(char a: x)
+    for(int a: x)
     {
-        if(n->children[int(a)] == nullptr)
+        if(n->children[a] == nullptr)
             return;
-        else
-            n = n->children[int(a)];
+        n = n->children[a];
     }
     for(int i = 0; i < n->top.size(); i++)
         T.push_back(n->top[i].s);
